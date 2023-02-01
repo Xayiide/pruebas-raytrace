9137e702b1aad8a4baf9c595de9c4857429a8105
Xayiide <wickedcheerio@protonmail.com>
2023-02-02 00:47:37 +0100

    progresito

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
+#include "ray.h"
 #include "vec3.h"
 
+vec3 color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+}
+
 int main() {
-    int i, j, ir, ig, ib;
+    int i, j, ir, ig, ib, u, v;
     int nx = 200;
     int ny = 100;
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
+
     for (j = ny - 1; j >= 0; j--) {
         for (i = 0; i < nx; i++) {
-            vec3 col(float(i) / float(nx),
-                     float(j)   / float(ny),
-                     0.6);
+            u = float(i) / float(nx);
+            v = float(j) / float(ny);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            vec3 col = color(r);
             ir = int(255.99 * col[0]);
             ig = int(255.99 * col[1]);
             ib = int(255.99 * col[2]); /* 51 */
